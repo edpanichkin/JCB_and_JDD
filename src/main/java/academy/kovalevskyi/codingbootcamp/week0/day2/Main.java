7d9c18ce7c6645a42181cc5f4decdc1c616fa511
default_name default_name <default@example.com>
2021-11-10 08:26:46 +0000

    new tree of projects

--- src/main/java/academy/kovalevskyi/codingbootcamp/week0/day2/Main.java
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week0/day2/Main.java
@@ -1,4 +1,6 @@
-class HelloWorld {
+package academy.kovalevskyi.codingbootcamp.week0.day2;
+
+public class Main {
   public static void main(String[] args) {
     System.out.println("Hi from Vim");
   }
