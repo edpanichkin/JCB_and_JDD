1a4f2c13a08aa910a0153b8647a19f282666d4b0
default_name default_name <default@example.com>
2021-11-10 10:33:33 +0000

    performed exec:java / changed .gitignore

--- src/main/java/academy/kovalevskyi/codingbootcamp/week0/day2/Main.java
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week0/day2/Main.java
@@ -2,6 +2,6 @@ package academy.kovalevskyi.codingbootcamp.week0.day2;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hi from Vim");
+    System.out.println("Hi from Vim!\n   also\nHi from Maven!");
   }
 }
