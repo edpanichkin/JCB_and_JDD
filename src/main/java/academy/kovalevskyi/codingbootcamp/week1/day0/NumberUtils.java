188ca10fecb004fcf3926723f42547e85e04f288
default_name default_name <default@example.com>
2021-11-16 03:58:48 +0000

    task w1d0 done / before review

--- src/main/java/academy/kovalevskyi/codingbootcamp/week1/day0/NumberUtils.java
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week1/day0/NumberUtils.java
@@ -44,27 +44,39 @@ public class NumberUtils {
   }
   
   public static double powerRecursive(int base, int power) {
-    return 0;
+    if (power > 0) {
+      return base * powerRecursive(base, power - 1);    
+    } 
+    if (power < 0) {
+      return 1 / powerRecursive(base, -1 * (power)); 
+    }
+    return 1;
   }  
 
   public static int fibRecursive(int index) {
-    return 0;    
+    if (index < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (index == 0) {
+      return 0;
+    } else if (index == 1) {
+      return 1;
+    } else  {
+      return fibRecursive(index - 1) + fibRecursive(index - 2);
+    }
   }
 
   public static int[] fibSequence(int length) {
-    if (length <= 0) {
+    if (length < 0) {
       throw new IllegalArgumentException();
+    }
+    if (length == 0) {
+      return new int[0];
     }
     int[] fibSequence;
     if (length == 1) {
       fibSequence = new int[length];
       fibSequence[0] = 1;
-      return fibSequence;
-    }
-    if (length == 2) {
-      fibSequence = new int[length];
-      fibSequence[0] = 1;
-      fibSequence[1] = 1;
       return fibSequence;
     }
     fibSequence = new int[length];
@@ -74,5 +86,65 @@ public class NumberUtils {
       fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
     }
     return fibSequence;
+  }
+
+  public static int sqrt(int target) {
+    if (target <= 0) {
+      throw new IllegalArgumentException();
+    }
+    for (int i = 1; i <= 46340; i++) {
+      if (power(i, 2) == target) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public static boolean isPrime(int target) {
+    System.out.println(target);    
+    if (target == 1) {
+      return false;
+    }
+    if (target <= 0 || target > Integer.MAX_VALUE) {
+      throw new IllegalArgumentException();
+    }
+    for (int i = 2; i < target; i++) {
+      if (target % i == 0 && i < target) {
+        return false;
+      } 
+    }
+    return true; 
+  }
+
+  public static int findNextPrime(int target) {
+    if (target < 0) {
+      throw new IllegalArgumentException();
+    }
+    if (target == 0) {
+      return 2;  
+    }
+    if (isPrime(target)) {
+      return target;
+    }
+    return findNextPrime(target + 1);
+  }
+
+  public static void sort(int[] target) {
+    if (target == null) {
+      return;
+    }
+    boolean isSorted = false;
+    int buf;
+    while (!isSorted) {
+      isSorted = true;
+      for (int i = 0; i < target.length - 1; i++) {
+        if (target[i] > target[i + 1]) {
+          isSorted = false;
+          buf = target[i];
+          target[i] = target[i + 1];
+          target[i + 1] = buf;
+        }
+      }
+    }
   }
 }
---
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week1/day0/Point.java
@@ -0,0 +1,68 @@
+package academy.kovalevskyi.codingbootcamp.week1.day0;
+
+public class Point implements Comparable<Point> {
+  private final int coordinateX;
+  private final int coordinateY;
+
+  public Point(final int coordinateX, final int coordinateY) {
+    this.coordinateX = coordinateX;
+    this.coordinateY = coordinateY;
+  }
+
+  public int getX() {
+    return this.coordinateX;  
+  }
+
+  public int getY() {
+    return this.coordinateY;  
+  }
+
+  public Point sum(final Point that) {
+    return new Point((that.getX() + this.getX()), (that.getY() + this.getY()));
+  }
+
+  public Point updateY(int newY) {
+    return new Point(this.getX(), newY);
+  }
+
+  public Point updateX(int newX) {
+    return new Point(newX, this.getY());
+  }
+
+  public int distanceTo(Point that) {
+    return (int) NumberUtils.power(this.getX() - that.getX(), 2) 
+      + (int) NumberUtils.power(this.getY() - that.getY(), 2);
+  } 
+
+  @Override
+  public int compareTo(Point o) {
+    int a = this.getX() + this.getY();
+    int b = o.getX() + o.getY();
+    if (a > b || a < b) {
+      return a - b;
+    }    
+    return 0;
+  } 
+  
+  @Override
+  public int hashCode() {
+    return this.getX() + this.getY();  
+  }
+
+  @Override
+  public String toString() {
+    return "Point{X: " + this.getX() + ", Y: " + this.getY() + "}";
+  } 
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Point point = (Point) o;
+    return this.getX() == point.getX() && this.getY() == point.getY();
+  }
+}
