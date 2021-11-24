f5490fe96c6f342c0fc5a18af78d21eb95431dfd
default_name default_name <default@example.com>
2021-11-24 02:14:48 +0000

    task w2d0 calculator -switch-express

--- src/main/java/academy/kovalevskyi/codingbootcamp/week2/day0/Calculator.java
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week2/day0/Calculator.java
@@ -1,41 +1,26 @@
 package academy.kovalevskyi.codingbootcamp.week2.day0;
-import academy.kovalevskyi.codingbootcamp.week1.day2.StringUtils;
 
 public class Calculator {
-
-  public static void main(String[] args) throws IllegalArgumentException {
+  public static void main(String[] args) {
     if(args.length != 3) {
       System.out.println("Please provide 3 input arguments, example: 2 + 3");
       return;
     }
-    long a = StringUtils.toInt(args[0].toCharArray());
-    long b = StringUtils.toInt(args[2].toCharArray());
- 
-    switch(args[1]) {
-      case "/":
-        if (b == 0) {
-          System.out.println("Division by zero is impossible!");
-        } else {
-          System.out.println("result: " + (double) a/b);
-        }
-        break;
-      case "+":
-        System.out.println("result: " +  (a + b));
-        break;
-      case "-":
-        System.out.println("result: " + (a - b));
-        break;
-      case "*":
-        System.out.println("result: " +  (a * b));
-        break;
-      case "%":
-        if (b == 0) {
-          System.out.println("Division by zero is impossible!");
-        } else {
-          System.out.println("result: " +  (a % b));
-        }
-        break;
-      default: throw new IllegalArgumentException();
+    Long a = Long.parseLong(args[0]);
+    Long b = Long.parseLong(args[2]);
+    String op = args[1];
+    if (b == 0 && ((op == "%") || (op == "/"))) { 
+      System.out.println("Division by zero is impossible!");
+      return;
     }
+    Number res = switch(op) {
+      case "/"  ->  (double) a / b;
+      case "+" -> (a + b);
+      case "-" -> (a - b);
+      case "*" -> a * b;
+      case "%" -> a % b;
+      default -> throw new IllegalArgumentException();
+    };
+    System.out.println("result: " + res);
   }
 }
--- src/main/java/academy/kovalevskyi/codingbootcamp/week2/day0/Numbers2.java
+++ src/main/java/academy/kovalevskyi/codingbootcamp/week2/day0/Numbers2.java
@@ -9,7 +9,20 @@ public class Numbers2 extends Numbers1 {
     int n = 10;
     int m = 3;
 //    int combCount = (int) NumberUtils.getFactorial(n) / (NumberUtils.getFactorial(n - m) * NumberUtils.getFactorial(m)); 
-    return null;
+    char[][] charArr = new char[120][3];
+    fillByZero(charArr);
+    int count = 12;
+    for (int i = 0; i < charArr.length; i++) {
+      char[] buf =  fillRowsByUniqueDigits(charArr[i], count);
+      if (buf.length != 0) {
+      System.out.print(" IN OK  ");
+      System.out.print(buf);
+      System.out.println("");
+        charArr[i] = buf;
+      }     
+      count++;
+    }
+    return charArr;
   }
 
   public static char[][] generateTuples() {
@@ -41,6 +54,34 @@ public class Numbers2 extends Numbers1 {
     return charArr;
   }
 
+  public static void fillByZero(char[][] tuple) {
+    for (int i = 0; i < tuple.length; i++) {
+      for (int j = 0; j < tuple[i].length; j++) {
+        tuple[i][j] = '0';
+      }
+    }
+  }
+  
+  public static char[] fillRowsByUniqueDigits(char[] row, int digit) {
+    char[] digitChar = Numbers1.convertToCharArray(digit);
+    System.arraycopy(digitChar, 0, row, row.length - digitChar.length,digitChar.length);
+      if ( row[0] ==  row[2]) 
+//        || (int) row[0] == (int) row[2]
+  //      || (int) row[1] == (int) row[2])
+ {
+
+System.out.print("bad ");
+System.out.print(row);
+        return new char[]{};
+      }    
+    return row;
+  }
+
+  public static char[] fillRowsByDigits(char[] row, int digit) {
+    char[] digitChar = Numbers1.convertToCharArray(digit);
+    System.arraycopy(digitChar, 0, row, row.length - digitChar.length,digitChar.length );
+    return row;
+  }
   public static char[] fillRowsByDigitsWithSpace(char[] row, int digit) {
     char[] digitChar = convertToCharArray(digit);
     System.arraycopy(digitChar, 0, row, row.length - digitChar.length,digitChar.length );
@@ -50,18 +91,4 @@ public class Numbers2 extends Numbers1 {
     return row;
   }
 
-    
-  public static void fillByZero(char[][] tuple) {
-    for (int i = 0; i < tuple.length; i++) {
-      for (int j = 0; j < tuple[i].length; j++) {
-        tuple[i][j] = '0';
-      }
-    }
-  }
-
-  public static char[] fillRowsByDigits(char[] row, int digit) {
-    char[] digitChar = Numbers1.convertToCharArray(digit);
-    System.arraycopy(digitChar, 0, row, row.length - digitChar.length,digitChar.length );
-    return row;
-  }
 }
